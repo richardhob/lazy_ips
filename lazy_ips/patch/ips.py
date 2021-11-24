60d5a5dbe170fb36dea0e2a33ea531df49e00230
Rick <richardhob@gmail.com>
2021-11-23 16:21:58 -0800

    Refactor: fike -> patch_file, add notes

--- lazy_ips/patch/ips.py
+++ lazy_ips/patch/ips.py
@@ -57,11 +57,11 @@ DATA_LEN_SIZE = 2  # bytes
 RLE_LEN_SIZE = 2  # bytes
 
 
-def read_patch_line(file):
+def read_patch_line(patch_file):
     ''' Read a line from the patch file, and decode the patch data.
 
     Args:
-        input_file (File): opened IPS patch file
+        patch_file (File): opened IPS patch file
 
     Returns:
         Named Tuple PatchLine(offset, patch_data)
@@ -69,7 +69,7 @@ def read_patch_line(file):
     Raises:
         IOError if the Offset or Data is formatted incorrectly
     '''
-    offset_raw = file.read(OFFSET_SIZE)
+    offset_raw = patch_file.read(OFFSET_SIZE)
     if len(offset_raw) != OFFSET_SIZE:
         raise IOError("not enough offset bytes read")
 
@@ -77,25 +77,25 @@ def read_patch_line(file):
         return None
 
     offset = unpack('>l', b'\x00' + offset_raw)[0]
-    data_len_raw = file.read(DATA_LEN_SIZE)
+    data_len_raw = patch_file.read(DATA_LEN_SIZE)
     if len(data_len_raw) != DATA_LEN_SIZE:
         raise IOError("not enough data len bytes read")
 
     data_len = unpack('>h', data_len_raw)[0]
 
     if data_len > 0:
-        data = file.read(data_len)
+        data = patch_file.read(data_len)
         if len(data) != data_len:
             raise IOError("not enough data bytes read")
         return PatchLine(offset, data)
 
     # RLE
-    rle_len_raw = file.read(RLE_LEN_SIZE)
+    rle_len_raw = patch_file.read(RLE_LEN_SIZE)
     if len(rle_len_raw) != RLE_LEN_SIZE:
         raise IOError("not enough rle data len bytes")
 
     rle_len = unpack('>h', rle_len_raw)[0]
-    rle_byte = file.read(1)
+    rle_byte = patch_file.read(1)
     if len(rle_byte) != 1:
         raise IOError("not enough rle data bytes")
 
@@ -103,11 +103,11 @@ def read_patch_line(file):
     return PatchLine(offset, data)
 
 
-def read_patch(file):
+def read_patch(patch_file):
     ''' Read the input patch file, and generate patch line data (offset, data).
 
     Args:
-        input_file (File): Opened patch file
+        patch_file (File): Opened patch file
 
     Generates:
         Data from patch file (offset, data)
@@ -116,13 +116,13 @@ def read_patch(file):
         IOError if Patch file is misformatted
     '''
     # Check marker.
-    data = file.read(PATCH_MARKER_SIZE)
+    data = patch_file.read(PATCH_MARKER_SIZE)
     if data != PATCH_MARKER:
         raise IOError("unknown format")
 
     eof = False
     while not eof:
-        data = read_patch_line(file)
+        data = read_patch_line(patch_file)
         if data:
             yield data
         else:
@@ -139,7 +139,9 @@ def apply_patch_line(image, patch_line):
     try:
         image.seek(patch_line.offset)
     except Exception:
-        # Go to the 0th line from the end of the file (EOF)
-        # why is this here? 
+        # Go to the 0th line from the end of
+        # the file (EOF). This would only happen
+        # if patch_line.offset is not a number,
+        # or if patch_line is invalid
         image.seek(0, 2)
     image.write(patch_line.data)
