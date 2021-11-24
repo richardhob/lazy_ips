0aa742930b81d4d01da6beb5ce4601324c985af7
Rick <richardhob@gmail.com>
2021-11-23 18:43:02 -0800

    Add more tests (ok coverage now)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -28,7 +28,56 @@ def simple_patch(tmp_path):
         output.write(b'PATCH')
         output.write(b'EOF')
 
-    return patch
+    return path
+
+@pytest.fixture(scope="function")
+def patch1(tmp_path):
+    path = tmp_path / "patch1.ips"
+
+    with open(path, 'wb') as output:
+        output.write(b'PATCH')
+
+        # Location, Size, Data
+        output.write(int(0).to_bytes(3, 'big'))
+        output.write(int(1).to_bytes(2, 'big'))
+        output.write(int(2).to_bytes(1, 'big'))
+
+        output.write(b'EOF')
+
+    return path
+
+@pytest.fixture(scope="function")
+def patch2(tmp_path):
+    path = tmp_path / "patch2.ips"
+
+    with open(path, 'wb') as output:
+        output.write(b'PATCH')
+
+        # Location, Size, Data
+        output.write(int(199).to_bytes(3, 'big'))
+        output.write(int(4).to_bytes(2, 'big'))
+        output.write(int(0xDEADBEEF).to_bytes(4, 'big'))
+
+        output.write(b'EOF')
+
+    return path
+
+@pytest.fixture(scope="function")
+def patch3(tmp_path):
+    path = tmp_path / "patch3.ips"
+
+    with open(path, 'wb') as output:
+        output.write(b'PATCH')
+
+        # Location, 0, RLE Size, RLE Data
+        output.write(int(0).to_bytes(3, 'big'))
+        output.write(int(0).to_bytes(2, 'big'))
+        output.write(int(800).to_bytes(2, 'big'))
+        output.write(int(0).to_bytes(1, 'big'))
+
+        output.write(b'EOF')
+
+    return path
 
 def call(image, patch):
     subprocess.run(
@@ -43,3 +92,29 @@ def test_cli_no_files(no_patch):
 def test_cli_no_patch(image, no_patch):
     with pytest.raises(subprocess.CalledProcessError):
         call(image, no_patch)
+
+def test_cli_simple_patch(image, simple_patch):
+    call(image, simple_patch)
+
+def test_cli_patch1(image, patch1):
+    call(image, patch1)
+
+    with open(image, 'rb') as input_file:
+        input_file.seek(0)
+        value = input_file.read(1)
+        assert value == int(2).to_bytes(1, 'big')
+
+def test_cli_patch2(image, patch2):
+    call(image, patch2)
+
+    with open(image, 'rb') as input_file:
+        input_file.seek(199)
+        value = input_file.read(4)
+        assert value == int(0xDEADBEEF).to_bytes(4, 'big')
+ 
+def test_cli_patch3(image, patch3):
+    call(image, patch3)
+
+    with open(image, 'rb') as input_file:
+        value = input_file.read(800)
+        assert value == bytes(800 * [0])
